3ec94aa45f9d46ed595b2f4cebb020bb0b2e3355
Jray <1075860716@qq.com>
2022-05-29 01:44:04 +0800

    ✨ feat: Finished user side.

--- frontend/charging-in-bupt-user/src/App.vue
+++ frontend/charging-in-bupt-user/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="floatButton" v-if="token != ''">
-    <el-popover placement="bottom" :width="200" trigger="click" class="gm">
+    <el-popover placement="bottom" :width="250" trigger="click" class="gm">
       <template #reference>
         <el-icon :size="25" color="white"><Setting /></el-icon>
       </template>
@@ -11,6 +11,8 @@
             <el-radio-button label="1" />
             <el-radio-button label="2" />
             <el-radio-button label="3" />
+            <el-radio-button label="4" />
+            <el-radio-button label="5" />
           </el-radio-group>
         </el-form-item>
       </el-form>
@@ -80,7 +82,6 @@
         </div>
         <div class="state-remain-time">现在您该：{{ curStep }}</div>
         <div class="state-remain-time">下一步：{{ nextStep }}</div>
-        <div class="state-remain-time">预计还需：{{ remainTime }}</div>
         <div class="state-remain-time">
           该模式前面还有：{{ remainCarNum }}辆车
         </div>
@@ -94,7 +95,7 @@
         <el-button
           class="reqCharge"
           type="primary"
-          @click="reqChargeDialog = true"
+          @click="reqChargeDialogFunc"
         >
           请求充电
         </el-button>
@@ -106,7 +107,7 @@
             type="primary"
             @click="i.handler"
             size="large"
-            :loading="buttonLoading[operations.indexOf(i)]"
+            :loading="buttonLoading[i.id]"
           >
             {{ i.name }}
           </el-button>
@@ -162,16 +163,71 @@
     </el-main>
   </el-container>
   <el-dialog v-model="reqChargeDialog" title="选择充电模式" width="90%" center>
-    <span>请选择您的充电模式</span>
+    <span>
+      <el-form :model="reqChargeForm" label-width="120px" label-position="top">
+        <el-form-item label="充电模式">
+          <el-radio-group v-model="reqChargeForm.charge_mode">
+            <el-radio-button label="快充" />
+            <el-radio-button label="慢充" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="请求充电量">
+          <el-input-number
+            v-model="reqChargeForm.require_amount"
+            :min="1"
+          />（单位：Ah）
+        </el-form-item>
+        <el-form-item label="电池容量">
+          <el-input-number
+            v-model="reqChargeForm.battery_size"
+            :min="1"
+          />（单位：Ah）
+        </el-form-item>
+      </el-form>
+    </span>
     <template #footer>
       <span class="dialog-footer">
-        <el-button type="primary" @click="reqChargeDialog = false"
-          >快充</el-button
-        >
-        <el-button type="primary" @click="reqChargeDialog = false"
-          >慢充</el-button
+        <el-button
+          type="primary"
+          @click="reqChargeFunc"
+          :loading="chargeReqLoading"
+          >请求</el-button
         >
         <el-button @click="reqChargeDialog = false">取消</el-button>
+      </span>
+    </template>
+  </el-dialog>
+  <el-dialog
+    v-model="reqChangeChargeDialog"
+    title="修改充电模式"
+    width="90%"
+    center
+  >
+    <span>
+      <el-form :model="reqChargeForm" label-width="120px" label-position="top">
+        <el-form-item label="充电模式">
+          <el-radio-group v-model="reqChargeForm.charge_mode">
+            <el-radio-button label="快充" />
+            <el-radio-button label="慢充" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="请求充电量">
+          <el-input-number
+            v-model="reqChargeForm.require_amount"
+            :min="1"
+          />（单位：Ah）
+        </el-form-item>
+      </el-form>
+    </span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button
+          type="primary"
+          @click="reqChangeChargeFunc"
+          :loading="chargeReqLoading"
+          >修改</el-button
+        >
+        <el-button @click="reqChangeChargeDialog = false">取消</el-button>
       </span>
     </template>
   </el-dialog>
@@ -190,39 +246,62 @@
   </el-dialog>
   <el-dialog v-model="showDetail" title="查看详单" width="90%" center>
     <span>
+      <div style="margin-bottom: 15px">
+        选择订单号：
+        <el-select v-model="selectedOrder" placeholder="选择订单">
+          <el-option
+            v-for="item in order_list"
+            :key="item.order_id"
+            :label="item.order_id"
+            :value="order_list.indexOf(item)"
+          />
+        </el-select>
+      </div>
       <el-descriptions border :column="1">
         <el-descriptions-item label="详单编号" align="center">
-          114514
+          {{ order_list[selectedOrder].order_id }}
         </el-descriptions-item>
         <el-descriptions-item label="详单生成时间" align="center">
-          1919-08-10 11:45:14
+          {{ order_list[selectedOrder].create_time }}
         </el-descriptions-item>
         <el-descriptions-item label="充电桩编号" align="center">
-          6
+          {{ order_list[selectedOrder].charger_id }}
         </el-descriptions-item>
         <el-descriptions-item label="充电电量" align="center">
-          120度
+          {{ order_list[selectedOrder].charged_amount }} Ah
         </el-descriptions-item>
         <el-descriptions-item label="充电时长" align="center">
-          2小时37分10秒
+          {{
+            order_list[selectedOrder].charged_time / 3600 >= 1
+              ? Math.floor(order_list[selectedOrder].charged_time / 3600) +
+                " 小时"
+              : ""
+          }}
+          {{
+            (order_list[selectedOrder].charged_time % 3600) / 60 >= 1
+              ? Math.floor(
+                  (order_list[selectedOrder].charged_time % 3600) / 60
+                ) + " 分钟"
+              : ""
+          }}
+          {{ order_list[selectedOrder].charged_time % 60 }} 秒
         </el-descriptions-item>
         <el-descriptions-item label="启动时间" align="center">
-          1919-08-10 11:45:14
+          {{ order_list[selectedOrder].begin_time }}
         </el-descriptions-item>
         <el-descriptions-item label="停止时间" align="center">
-          1919-08-10 11:45:14
+          {{ order_list[selectedOrder].end_time }}
         </el-descriptions-item>
         <el-descriptions-item label="充电费用" align="center">
-          10.35元
+          {{ order_list[selectedOrder].charging_cost }} 元
         </el-descriptions-item>
         <el-descriptions-item label="服务费用" align="center">
-          1.03元
+          {{ order_list[selectedOrder].service_cost }} 元
         </el-descriptions-item>
         <el-descriptions-item label="总费用" align="center">
-          11.38元
+          {{ order_list[selectedOrder].total_cost }} 元
         </el-descriptions-item>
       </el-descriptions>
-      <div style="color: #909399; padding-top: 5px">*仅显示上一个详单</div>
     </span>
     <template #footer>
       <span class="dialog-footer">
@@ -233,41 +312,136 @@
 </template>
 
 <script>
-import { ElMessage } from "element-plus";
+import { ElMessage, ElLoading } from "element-plus";
+import axios from "axios";
 export default {
   name: "App",
-  setup() {},
+  created() {
+    if (localStorage.getItem("token")) {
+      const token = localStorage.getItem("token");
+      const loading = ElLoading.service({
+        fullscreen: true,
+        text: "正在加载...",
+        background: "rgba(255, 255, 255, 1)",
+      });
+      setTimeout(() => {
+        axios
+          .get("/api/user/preview_queue", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((res) => {
+            if (res.data.code === 0) {
+              const stateMap = {
+                NOTCHARGING: 0,
+                WAITINGSTAGE1: 1,
+                WAITINGSTAGE2: 2,
+                CHARGING: 3,
+                CHANGEMODEREQUEUE: 4,
+                FAULTREQUEUE: 5,
+              };
+              this.chargingNo = res.data.data.charge_id;
+              this.remainCarNum = res.data.data.queue_len;
+              this.curStateId = stateMap[res.data.data.cur_state];
+              this.place = res.data.data.place;
+              loading.close();
+              ElMessage.success("加载成功");
+            } else {
+              ElMessage.error(res.data.message);
+              loading.close();
+            }
+          })
+          .catch((err) => {
+            ElMessage.error(err);
+            loading.close();
+          });
+      }, 0);
+      this.startTimer();
+    }
+  },
   data() {
     const allOpt = [
       {
+        id: 0,
         name: "查看详单",
         handler: () => {
           this.buttonLoading[0] = true;
-          setTimeout(() => {
-            this.buttonLoading[0] = false;
-            this.showDetail = true;
-          }, 1000);
+          axios
+            .get("/api/user/query_order_detail")
+            .then((res) => {
+              if (res.data.code === 0) {
+                this.buttonLoading[0] = false;
+                this.order_list = res.data.data;
+                this.selectedOrder = 0;
+                if (this.order_list.length === 0) {
+                  ElMessage.warning("暂无详单");
+                  this.buttonLoading[0] = false;
+                } else {
+                  this.showDetail = true;
+                }
+              } else {
+                this.buttonLoading[0] = false;
+                ElMessage.error(res.data.message);
+              }
+            })
+            .catch((err) => {
+              this.buttonLoading[0] = false;
+              ElMessage.error(err);
+            });
         },
       },
       {
+        id: 1,
         name: "修改充电请求",
         handler: () => {
-          ElMessage.success("修改充电请求");
+          this.reqChangeChargeDialogFunc();
         },
       },
       {
+        id: 2,
         name: "取消充电",
         handler: () => {
-          ElMessage.success("取消充电");
+          this.buttonLoading[2] = true;
+          axios
+            .get("/api/user/end_charging_request")
+            .then((res) => {
+              if (res.data.code === 0) {
+                ElMessage.success("取消成功");
+              } else {
+                ElMessage.error(res.data.message);
+              }
+              this.buttonLoading[2] = false;
+            })
+            .catch((err) => {
+              ElMessage.error(err);
+              this.buttonLoading[2] = false;
+            });
         },
       },
       {
+        id: 3,
         name: "完成充电",
         handler: () => {
-          ElMessage.success("完成充电");
+          this.buttonLoading[3] = true;
+          axios
+            .get("/api/user/end_charging_request")
+            .then((res) => {
+              if (res.data.code === 0) {
+                ElMessage.success("完成充电");
+              } else {
+                ElMessage.error(res.data.message);
+              }
+              this.buttonLoading[3] = false;
+            })
+            .catch((err) => {
+              ElMessage.error(err);
+              this.buttonLoading[3] = false;
+            });
         },
       },
       {
+        id: 4,
         name: "查看费用规则",
         handler: () => {
           this.showFee = true;
@@ -276,16 +450,32 @@ export default {
     ];
     const operations = [0, 1, 2, 4].map((i) => allOpt[i]);
     return {
-      nowCharging: true,
-      chargingNo: "F114",
-      curStep: "在排队区等候",
-      curStateId: 1,
-      curState: "正在排队区等候",
-      nextStep: "进入充电等候区",
-      remainTime: "51分钟",
+      timer: null,
+      nowCharging: false,
+      chargingNo: "",
+      curStep: "",
+      curStateId: 0,
+      curState: "",
+      nextStep: "",
+      order_list: [
+        {
+          begin_time: "1986-09-15 14:43:40",
+          create_time: "1995-12-13 04:15:21",
+          service_cost: 87,
+          charged_time: 1605705295379,
+          total_cost: 92,
+          order_id: "26",
+          charged_amount: 17,
+          charging_cost: 96,
+          end_time: "2016-07-30 23:59:14",
+        },
+      ],
+      place: "",
       remainCarNum: 4,
       token: localStorage.getItem("token") ?? "",
+      chargeReqLoading: false,
       reqChargeDialog: false,
+      reqChangeChargeDialog: false,
       showFee: false,
       showDetail: false,
       allOpt: allOpt,
@@ -338,6 +528,12 @@ export default {
           fee: "0.4元/度",
         },
       ],
+      selectedOrder: 0,
+      reqChargeForm: {
+        charge_mode: "快充",
+        require_amount: 0,
+        battery_size: 0,
+      },
     };
   },
   methods: {
@@ -345,18 +541,78 @@ export default {
       this.loginOrRegister = !this.loginOrRegister;
     },
     login() {
-      ElMessage.success("登录成功");
-      localStorage.setItem("token", "123");
-      this.token = "123";
+      const loading = ElLoading.service({
+        fullscreen: true,
+        text: "登录中...",
+        background: "rgba(255, 255, 255, 1)",
+      });
+      axios
+        .post("/api/user/login", {
+          username: this.loginForm.username,
+          password: this.loginForm.password,
+        })
+        .then((res) => {
+          if (res.data.code === 0) {
+            const token = res.data.data.token;
+            axios
+              .get("/api/user/preview_queue", {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              })
+              .then((res) => {
+                if (res.data.code === 0) {
+                  const stateMap = {
+                    NOTCHARGING: 0,
+                    WAITINGSTAGE1: 1,
+                    WAITINGSTAGE2: 2,
+                    CHARGING: 3,
+                    CHANGEMODEREQUEUE: 4,
+                    FAULTREQUEUE: 5,
+                  };
+                  this.token = token;
+                  localStorage.setItem("token", token);
+                  this.chargingNo = res.data.data.charge_id;
+                  this.remainCarNum = res.data.data.queue_len;
+                  this.curStateId = stateMap[res.data.data.cur_state];
+                  this.place = res.data.data.place;
+                  loading.close();
+                  ElMessage.success("登录成功");
+                  this.startTimer();
+                } else {
+                  ElMessage.error(res.data.message);
+                  loading.close();
+                }
+              })
+              .catch((err) => {
+                ElMessage.error(err);
+                loading.close();
+              });
+          } else {
+            ElMessage.error(res.data.message);
+          }
+        });
     },
     register() {
-      ElMessage.success("注册成功");
-      localStorage.setItem("token", "123");
-      this.token = "123";
+      axios
+        .post("/api/user/register", {
+          username: this.loginForm.username,
+          password: this.loginForm.password,
+          re_password: this.loginForm.passwordAgain,
+        })
+        .then((res) => {
+          if (res.data.code === 0) {
+            ElMessage.success("注册成功，请登录");
+            this.loginOrRegister = true;
+          } else {
+            ElMessage.error(res.data.message);
+          }
+        });
     },
     logout() {
       ElMessage.success("退出成功");
       localStorage.removeItem("token");
+      this.stopTimer();
       this.token = "";
       this.loginOrRegister = true;
       this.loginForm = {
@@ -365,19 +621,141 @@ export default {
         passwordAgain: "",
       };
     },
+    reqChargeDialogFunc() {
+      this.reqChargeForm = {
+        charge_mode: "快充",
+        require_amount: 0,
+        battery_size: 0,
+      };
+      this.reqChargeDialog = true;
+    },
+    reqChargeFunc() {
+      this.chargeReqLoading = true;
+      axios
+        .post("/api/user/submit_charging_request", {
+          charge_mode: this.reqChargeForm.charge_mode,
+          require_amount: this.reqChargeForm.require_amount,
+          battery_size: this.reqChargeForm.battery_size,
+        })
+        .then((res) => {
+          if (res.data.code === 0) {
+            ElMessage.success("请求充电成功");
+            this.chargeReqLoading = false;
+            this.reqChargeDialog = false;
+          } else {
+            ElMessage.error(res.data.message);
+            this.chargeReqLoading = false;
+          }
+        });
+    },
+    reqChangeChargeDialogFunc() {
+      this.reqChangeChargeForm = {
+        charge_mode: "快充",
+        require_amount: 0,
+        battery_size: 0,
+      };
+      this.reqChangeChargeDialog = true;
+    },
+    reqChangeChargeFunc() {
+      this.chargeReqLoading = true;
+      axios
+        .post("/api/user/edit_charging_request", {
+          charge_mode: this.reqChangeChargeForm.charge_mode,
+          require_amount: this.reqChangeChargeForm.require_amount,
+        })
+        .then((res) => {
+          if (res.data.code === 0) {
+            ElMessage.success("修改充电请求成功");
+            this.chargeReqLoading = false;
+            this.reqChangeChargeDialog = false;
+          } else {
+            ElMessage.error(res.data.message);
+            this.chargeReqLoading = false;
+          }
+        });
+    },
+    startTimer() {
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
+      this.timer = setInterval(() => {
+        this.refreshState();
+      }, 5000);
+    },
+    stopTimer() {
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
+    },
+    refreshState() {
+      const token = this.token;
+      if (token) {
+        axios
+          .get("/api/user/preview_queue", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((res) => {
+            if (res.data.code === 0) {
+              const stateMap = {
+                NOTCHARGING: 0,
+                WAITINGSTAGE1: 1,
+                WAITINGSTAGE2: 2,
+                CHARGING: 3,
+                CHANGEMODEREQUEUE: 4,
+                FAULTREQUEUE: 5,
+              };
+              this.chargingNo = res.data.data.charge_id;
+              this.remainCarNum = res.data.data.queue_len;
+              this.curStateId = stateMap[res.data.data.cur_state];
+              this.place = res.data.data.place;
+              ElMessage.success("刷新成功");
+            } else {
+              ElMessage.error(res.data.message);
+            }
+          })
+          .catch((err) => {
+            ElMessage.error(err);
+          });
+      }
+    },
   },
   watch: {
-    curStateId(val) {
-      if (this.curStateId == 0) {
+    curStateId(val, old) {
+      console.log(old, val);
+      // if (old != 0 && val != 0) {
+      //   ElMessageBox.alert("状态已更新，请注意您需要做的动作", "状态更新提醒", {
+      //     confirmButtonText: "好的",
+      //   });
+      // }
+      if (val == 0) {
         this.nowCharging = false;
       } else {
         this.nowCharging = true;
       }
       const stateMap = {
         0: ["", "", "", [0, 4]],
-        1: ["正在排队区等候", "进入充电区等候", "在排队区等候", [0, 1, 2, 4]],
-        2: ["正在充电区等候", "进行充电", "进入充电区并等候", [0, 2, 4]],
+        1: ["正在等候区等候", "进入充电区等候", "在等候区等候", [0, 1, 2, 4]],
+        2: [
+          "正在充电区等候",
+          "进行充电",
+          `进入${this.place}号充电桩并等候`,
+          [0, 2, 4],
+        ],
         3: ["正在充电", "完成充电", "进行充电", [0, 3, 4]],
+        4: [
+          "正在等候区等候",
+          "进入充电区等候",
+          "由于修改了充电请求，请重新进入等候区等候",
+          [0, 1, 2, 4],
+        ],
+        5: [
+          "正在充电区等候",
+          "进行充电",
+          `由于充电桩故障，请进入${this.place}号充电桩并等候`,
+          [0, 2, 4],
+        ],
       };
       this.curState = stateMap[val][0];
       this.nextStep = stateMap[val][1];
@@ -438,8 +816,9 @@ export default {
   height: 100px;
   border-radius: 50%;
   font-size: 16px;
+  margin: 25px;
 }
 .el-dialog {
-  max-width: 600px;
+  max-width: 450px;
 }
 </style>
