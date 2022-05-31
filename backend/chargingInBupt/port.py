48c7c9a483cd91007f162831a32c93e177186dd8
2019211608 <853921669@qq.com>
2022-05-31 23:15:01 +0800

    part of port finished

--- backend/chargingInBupt/port.py
+++ backend/chargingInBupt/port.py
@@ -2,12 +2,15 @@ from sanic import Sanic, json
 from sanic.response import text
 
 from chargingInBupt.auth import authorized, generate_token, authorized_admin, get_username
-from chargingInBupt.orm import User, session
+from chargingInBupt.orm import User, session, ChargeRequest
 from chargingInBupt.json_validate import json_validate
 from chargingInBupt.json_schema import *
+from chargingInBupt.config import CONFIG
 
 
 app = Sanic("Charging_in_BUPT")
+F_wait_list = []
+T_wait_list = []
 
 
 @app.post('/user/login')
@@ -83,14 +86,49 @@ async def submit_charging_request(request):
     require_amount = request.json.get('require_amount')
     battery_size = request.json.get('battery_size')
     # TODO(1): 处理，获取 charge_id
-    # 判断是否不在充电状态
-    # 插入对应队列
-    # 生成充电请求，插入数据库
-    # 返回排队id
-    # 如果等待区不为空要调度
-    success = None
-    error_msg = None
-    charge_id = None
+    # 判断是否不在充电状态:没有充电记录或者不存在待充电请求则代表不在充电状态
+    record = session.query(ChargeRequest).filter(ChargeRequest.user_id == user.id).first()
+    undo_record = session.query(ChargeRequest).filter(ChargeRequest.user_id == user.id and ChargeRequest.state in [0,1,2]).first()
+    charge_time = None
+    if record is None or undo_record is None:
+        # 请求id
+        record_num = session.query(ChargeRequest).count()
+        request_id = str(record_num + 1)
+        # 插入对应队列
+        if len(F_wait_list)+len(T_wait_list) < 6:
+            if charge_mode == "F":
+                F_wait_list.append(request_id)
+                charge_time = require_amount/CONFIG['cfg']['F_power']
+                charge_id = "F"+str(len(F_wait_list))
+            elif charge_mode == "T":
+                T_wait_list.append(request_id)
+                charge_time = require_amount/CONFIG['cfg']['T_power']
+                charge_id = "T"+str(len(T_wait_list))
+            else:
+                charge_id = None
+            # 生成充电请求，插入数据库
+            charge_request = ChargeRequest(id=request_id, state=0, user_id=user.id, charge_mode=charge_mode, require_amount=float(require_amount), charge_time=charge_time, battery_size=float(battery_size), charge_id=charge_id)
+            session.add(charge_request)
+            session.commit()
+            success = True
+            error_msg = None
+            # 如果等待区不为空要调度:似乎只需要调度程序对F_wait_list，T_wait_list不断进行调度即可
+            '''
+            if charge_mode == "F":
+                # 调度函数（充电请求id）:充电请求id:F_wait_list[0]
+                pass
+            elif charge_mode == "T":
+                # 调度函数（充电请求id）:充电请求id:T_wait_list[0]
+                pass
+            '''
+        else:
+            success = False
+            error_msg = "请求失败，等候区已满。"
+            charge_id = None
+    else:
+        success = False
+        error_msg = "请求失败，还有待完成充电请求。"
+        charge_id = None
     if success:
         return json({
             "code": 0,
@@ -105,6 +143,7 @@ async def submit_charging_request(request):
             "message": error_msg
         })
 
+
 @app.post('/user/edit_charging_request')
 @json_validate(edit_charging_request_json_schema)
 @authorized()
@@ -114,10 +153,41 @@ async def edit_charging_request(request):
     require_amount = request.json.get('require_amount')
     # TODO(1): 处理，修改充电请求
     # 判断是否可以修改
-    # 修改后数据写入数据库
-    # 放入队列最后
-    success = None
-    error_msg = None
+    record = session.query(ChargeRequest).filter(ChargeRequest.user_id == user.id and ChargeRequest.state == 0).first()
+    # 存在还在等候区的
+    if record is not None:
+        # 如果充电模式有修改则放到队列最后
+        if record.charge_mode != charge_mode:
+            if record.charge_mode == "F":
+                F_wait_list.remove(record.id)
+                # 让所有排在该请求后面的请求的排队号-1
+                for request_id in F_wait_list:
+                    session.query(ChargeRequest).filter(ChargeRequest.id == request_id and int(ChargeRequest.charge_id[1:])>int(record.charge_id[1:])).update({
+                        "charge_id": "F" + str(int(ChargeRequest.charge_id[1:])-1)
+                    })
+                charge_id = "F" + str(F_wait_list)
+                F_wait_list.append(record.id)
+            elif record.charge_mode == "T":
+                T_wait_list.remove(record.id)
+                # 让所有排在该请求后面的请求的排队号-1
+                for request_id in T_wait_list:
+                    session.query(ChargeRequest).filter(ChargeRequest.id == request_id and int(ChargeRequest.charge_id[1:])>int(record.charge_id[1:])).update({
+                        "charge_id": "F" + str(int(ChargeRequest.charge_id[1:])-1)
+                    })
+                charge_id = "T" + str(F_wait_list)
+                T_wait_list.append(record.id)
+        # 修改后数据写入数据库
+        session.query(ChargeRequest).filter(ChargeRequest.id == record.id).update({
+            "charge_mode": charge_mode,
+            "require_amount": require_amount,
+            "charge_id": charge_id
+        })
+        session.commit()
+        success = True
+        error_msg = None
+    else:
+        success = False
+        error_msg = "修改失败，车辆不在等候区。"
     if success:
         return json({
             "code": 0,
@@ -128,6 +198,7 @@ async def edit_charging_request(request):
             "code": -1,
             "message": error_msg
         })
+
 
 @app.get('/user/end_charging_request')
 @authorized()
@@ -177,13 +248,43 @@ async def preview_queue(request):
     user = session.query(User).filter(User.username == get_username(request)).first()
     # TODO(1): 处理，获取排队详情
     # 读取数据库
-    # 算前方车辆
-    success = None
+    record = session.query(ChargeRequest).filter(ChargeRequest.user_id == user.id and ChargeRequest.state in [0,1,2]).first()
+    # 若存在还未结束的充电请求
+    if record is not None:
+        charge_id = record.charge_id
+        info = ["WAITINGSTAGE1","WAITINGSTAGE2","CHARGING"]
+        cur_state = info[record.state]
+        if record.state == 0:
+            place = "WAITINGPLACE"
+        elif record.state in [1,2]:
+            place = record.charge_pile_id
+        else:
+            place = None
+        # 算前方车辆
+        if record.state == 0:
+            queue_len = (record.charge_id[1:])
+        elif record.state == 1:
+            # queue_len = 对应record.charge_pile_id充电桩前面车的数量
+            queue_len = None
+            pass
+        elif record.state == 2:
+            queue_len = 0
+        else:
+            queue_len = None
+    else:
+        cur_state = "NOTCHARGING"
+        charge_id = None
+        queue_len = None
+        cur_state = None
+        place = None
+    success = True
     error_msg = None
-    charge_id = None
-    queue_len = None
-    cur_state = None
-    place = None
+    # success = None
+    # error_msg = None
+    # charge_id = None
+    # queue_len = None
+    # cur_state = None
+    # place = None
     if success:
         return json({
             "code": 0,
